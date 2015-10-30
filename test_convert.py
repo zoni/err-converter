6edeb8dc08bb24118339ba2e8fa1d1a3d81e08b8
Nick Groenen <nick@groenen.me>
2015-10-30 23:59:30 +0100

    Target Err 3.1.0+

--- test_convert.py
+++ test_convert.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 from os.path import abspath, dirname
-from errbot.backends.test import testbot, push_message, pop_message
+from errbot.backends.test import testbot, FullStackTest
 from convert import Converter
 
 extra_plugin_dir = dirname(abspath(__name__))
@@ -8,19 +8,19 @@ extra_plugin_dir = dirname(abspath(__name__))
 
 class TestTemperatureConversions(object):
     def test_convert_botcmd(self, testbot):
-        push_message("!convert 1 kilometer to meters")
-        assert pop_message() == "1 kilometer = 1000 meter"
-        push_message("!convert 1 kilometer to yards")
-        assert pop_message() == "1 kilometer = 1093.61 yard"
+        testbot.bot.push_message("!convert 1 kilometer to meters")
+        assert testbot.bot.pop_message() == "1 kilometer = 1000 meter"
+        testbot.bot.push_message("!convert 1 kilometer to yards")
+        assert testbot.bot.pop_message() == "1 kilometer = 1093.61 yard"
 
-        push_message("!convert a kilometer to yards")
-        m = pop_message()
+        testbot.bot.push_message("!convert a kilometer to yards")
+        m = testbot.bot.pop_message()
         assert (
             m == "I don't know how to convert that (could not convert string to float: 'a')."
             or m == "I don't know how to convert that (could not convert string to float: a)."
         )
-        push_message("!convert 1 foo to bar")
-        assert pop_message() == "I don't know how to convert that ('foo' is not defined in the unit registry)."
+        testbot.bot.push_message("!convert 1 foo to bar")
+        assert testbot.bot.pop_message() == "I don't know how to convert that ('foo' is not defined in the unit registry)."
 
     def test_temperature_regex(self, testbot):
         say_expect = [
@@ -41,14 +41,14 @@ class TestTemperatureConversions(object):
         ]
 
         for item in say_expect:
-            push_message(item[0])
-            assert pop_message() == item[1]
+            testbot.bot.push_message(item[0])
+            assert testbot.bot.pop_message() == item[1]
 
-        push_message("It's 30 c today")
-        push_message("It's 30 f today")
-        push_message("It's 30 degrees celvin (I know Kelvin should be with a K)")
+        testbot.bot.push_message("It's 30 c today")
+        testbot.bot.push_message("It's 30 f today")
+        testbot.bot.push_message("It's 30 degrees celvin (I know Kelvin should be with a K)")
 
         # None of the above should match so response to this should be
-        # first item returned by pop_message().
-        push_message("!echo trick-to-avoid-wait-for-empty-queue")
-        assert pop_message() == "trick-to-avoid-wait-for-empty-queue"
+        # first item returned by testbot.bot.pop_message().
+        testbot.bot.push_message("!echo trick-to-avoid-wait-for-empty-queue")
+        assert testbot.bot.pop_message() == "trick-to-avoid-wait-for-empty-queue"
